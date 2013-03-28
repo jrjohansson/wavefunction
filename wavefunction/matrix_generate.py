41c3028967eca309b510d683df78d60f7ea742f8
Robert Johansson <jrjohansson@gmail.com>
2013-03-28 16:11:44 +0900

    new notebooks and files

---
+++ wavefunction/matrix_generate.py
@@ -0,0 +1,48 @@
+#
+# 
+#
+# J Robert Johansson, <robert@riken.jp>
+#
+from scipy import *
+
+#
+# Generate matrix representation of Kronecker-delta(i-x,j-y)
+#
+def kd(nstates, x, y):
+
+    D = zeros((nstates, nstates), dtype=float)
+    
+    for i in range(0, nstates):
+        for j in range(0, nstates):
+            
+            if (i-x) == (j-y):
+                D[i,j] = 1
+            else:
+                D[i,j] = 0    
+
+    return D
+
+#
+# Generate matrix representation of Kronecker-delta product
+# kd(a,c)kd(b,d-X)
+# where a,b,c,d = -N:N and X is a constant
+#
+def kdp(nstates, X):
+
+    N = (nstates-1)/2;
+    D = zeros((nstates**2, nstates**2), dtype=float)
+    k = arange(-N,N+1)
+        
+    for aidx in range(0,nstates):
+        for bidx in range(0,nstates):
+            for cidx in range(0,nstates):
+                for didx in range(0,nstates):
+                    I = (aidx) * nstates + bidx;
+                    J = (cidx) * nstates + didx;
+                    
+                    if (k[aidx] == k[cidx]) & (k[bidx] == k[didx]+X):
+                        D[I,J] = 1
+                    else:
+                        D[I,J] = 0
+    
+    return D
---
+++ wavefunction/matrix_recast.py
@@ -0,0 +1,44 @@
+#
+#
+#
+
+from scipy import *
+
+#
+# Create M-matrix
+#
+def recastMtoT(nstates, a, b):
+
+    M = zeros((nstates**2,nstates**2), dtype=float)
+
+    for i in range(0,nstates):
+       for j in range(0,nstates):
+            for p in range(0,nstates):
+                for q in range(0,nstates):
+                    I = (i) * nstates + j;
+                    J = (p) * nstates + q;
+                    M[I,J] = a[i,p]*b[q,j];
+    
+    return M
+
+#
+# Create M-matrix
+#
+def recastMtoV(nstates, M):
+    v = zeros(nstates**2).astype(Float)
+    for i in range(0,nstates):
+        for j in range(0,nstates):
+            v[i*nstates+j] = M[i,j]
+    return v
+
+#
+# Create M-matrix
+#
+def recastVtoM(nstates, v):
+
+    M = zeros((nstates,nstates)).astype(Float)
+    for i in range(0,nstates):
+        for j in range(0,nstates):
+             M[i,j] = v[i*nstates+j] 
+        
+    return M

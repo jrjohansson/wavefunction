fed93a5fa27a866ad4a0dac9428a74ee9c3a519e
Robert Johansson <jrjohansson@gmail.com>
2013-03-28 16:10:12 +0900

    updated notebooks

--- wavefunction/__init__.py
+++ wavefunction/__init__.py
@@ -4,3 +4,6 @@ from wavefunction.potential import *
 from wavefunction.schrodinger_matrix import *
 from wavefunction.misc import *
 from wavefunction.nummath import *
+
+from matrix_recast import *
+from matrix_generate import *

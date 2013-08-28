02a5291a534389d59f046657976dad4f43350a60
Robert Johansson <jrjohansson@gmail.com>
2013-08-28 12:39:03 +0900

    added __version__ in __init__

--- wavefunction/__init__.py
+++ wavefunction/__init__.py
@@ -1,10 +1,18 @@
 """
 Python package for calculating wavefunctions for 1D and 2D potentials.
 """
-from wavefunction.potential import *
-from wavefunction.schrodinger_matrix import *
+
+# utilities
 from wavefunction.misc import *
 from wavefunction.nummath import *
+from wavefunction.version import version as __version__
+
+# 1D problems
+from wavefunction.potential import *
+from wavefunction.wavefunction1d import *
+
+# 2D problems
+from wavefunction.wavefunction2d import *
 
 from wavefunction.matrix_recast import *
 from wavefunction.matrix_generate import *
